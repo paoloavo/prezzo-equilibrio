5d831ce2a776037f517ea4b6388053a3d3816f84
paolo avogadro <paoloavo170207@gmail.com>
2025-11-25 18:41:41 +0100

    prezzoequilibrio1.0

--- prezzo equilibrio/Form1.Designer.cs
+++ prezzo equilibrio/Form1.Designer.cs
@@ -2,15 +2,23 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Variabile di progettazione necessaria.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Pulire le risorse in uso.
-        /// </summary>
-        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        private System.Windows.Forms.TextBox txtDOrigine;
+        private System.Windows.Forms.TextBox txtDCoeff;
+        private System.Windows.Forms.TextBox txtOCostante;
+        private System.Windows.Forms.TextBox txtOCoeff;
+        private System.Windows.Forms.TextBox txtOEsponente;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Button btnCalcola;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -19,22 +27,96 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Codice generato da Progettazione Windows Form
-
-        /// <summary>
-        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
-        /// il contenuto del metodo con l'editor di codice.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+            this.txtDOrigine = new System.Windows.Forms.TextBox();
+            this.txtDCoeff = new System.Windows.Forms.TextBox();
+            this.txtOCostante = new System.Windows.Forms.TextBox();
+            this.txtOCoeff = new System.Windows.Forms.TextBox();
+            this.txtOEsponente = new System.Windows.Forms.TextBox();
 
-        #endregion
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+
+            this.btnCalcola = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+
+            // ------------------- LABELS -------------------
+            this.label1.Text = "Domanda - Origine:";
+            this.label1.Location = new System.Drawing.Point(20, 20);
+
+            this.label2.Text = "Domanda - Coeff:";
+            this.label2.Location = new System.Drawing.Point(20, 60);
+
+            this.label3.Text = "Offerta - Costante:";
+            this.label3.Location = new System.Drawing.Point(250, 20);
+
+            this.label4.Text = "Offerta - Coeff:";
+            this.label4.Location = new System.Drawing.Point(250, 60);
+
+            this.label5.Text = "Offerta - Esponente:";
+            this.label5.Location = new System.Drawing.Point(250, 100);
+
+            // ------------------- TEXTBOX -------------------
+            this.txtDOrigine.Location = new System.Drawing.Point(150, 20);
+            this.txtDOrigine.Size = new System.Drawing.Size(70, 23);
+            this.txtDOrigine.Text = "90";
+
+            this.txtDCoeff.Location = new System.Drawing.Point(150, 60);
+            this.txtDCoeff.Size = new System.Drawing.Size(70, 23);
+            this.txtDCoeff.Text = "4";
+
+            this.txtOCostante.Location = new System.Drawing.Point(370, 20);
+            this.txtOCostante.Size = new System.Drawing.Size(70, 23);
+            this.txtOCostante.Text = "10";
+
+            this.txtOCoeff.Location = new System.Drawing.Point(370, 60);
+            this.txtOCoeff.Size = new System.Drawing.Size(70, 23);
+            this.txtOCoeff.Text = "0.01";
+
+            this.txtOEsponente.Location = new System.Drawing.Point(370, 100);
+            this.txtOEsponente.Size = new System.Drawing.Size(70, 23);
+            this.txtOEsponente.Text = "3";
+
+            // ------------------- BOTTONE -------------------
+            this.btnCalcola.Text = "Calcola";
+            this.btnCalcola.Location = new System.Drawing.Point(20, 100);
+            this.btnCalcola.Size = new System.Drawing.Size(200, 35);
+            this.btnCalcola.Click += new System.EventHandler(this.btnCalcola_Click);
+
+            // ------------------- CHART -------------------
+            this.chart1.Location = new System.Drawing.Point(20, 150);
+            this.chart1.Size = new System.Drawing.Size(600, 350);
+
+            // ------------------- FORM -------------------
+            this.ClientSize = new System.Drawing.Size(650, 520);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+
+            this.Controls.Add(this.txtDOrigine);
+            this.Controls.Add(this.txtDCoeff);
+            this.Controls.Add(this.txtOCostante);
+            this.Controls.Add(this.txtOCoeff);
+            this.Controls.Add(this.txtOEsponente);
+
+            this.Controls.Add(this.btnCalcola);
+            this.Controls.Add(this.chart1);
+
+            this.Text = "Domanda e Offerta";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+        }
     }
 }
+
+      
 
--- prezzo equilibrio/Form1.cs
+++ prezzo equilibrio/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace prezzo_equilibrio
 {
@@ -17,5 +17,39 @@ namespace prezzo_equilibrio
         {
             InitializeComponent();
         }
+
+        private void btnCalcola_Click(object sender, EventArgs e)
+        {
+            // Parametri inseriti dall'utente
+            double dOrigine = double.Parse(txtDOrigine.Text);
+            double dCoeff = double.Parse(txtDCoeff.Text);
+
+            double oCostante = double.Parse(txtOCostante.Text);
+            double oCoeff = double.Parse(txtOCoeff.Text);
+            double oEsponente = double.Parse(txtOEsponente.Text);
+
+            // Pulizia grafico
+            chart1.Series.Clear();
+
+            Series domanda = new Series("Domanda");
+            domanda.ChartType = SeriesChartType.Line;
+
+            Series offerta = new Series("Offerta");
+            offerta.ChartType = SeriesChartType.Line;
+
+            // Calcolo valori per q = 0..20
+            for (int q = 0; q <= 20; q++)
+            {
+                double dValue = dOrigine - dCoeff * q;
+                double oValue = oCostante + oCoeff * Math.Pow(q, oEsponente);
+
+                domanda.Points.AddXY(q, dValue);
+                offerta.Points.AddXY(q, oValue);
+            }
+
+            chart1.Series.Add(domanda);
+            chart1.Series.Add(offerta);
+        }
     }
 }
+
