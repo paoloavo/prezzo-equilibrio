8f557c0d0690f77efe67d3c3c93c538c90566615
avogadro.21097 <avogadro.21097@LABI1-13PC-21>
2025-11-26 13:17:18 +0100

    ese2.0

--- prezzo equilibrio/Form1.Designer.cs
+++ prezzo equilibrio/Form1.Designer.cs
@@ -4,118 +4,235 @@
     {
         private System.ComponentModel.IContainer components = null;
 
-        private System.Windows.Forms.TextBox txtDOrigine;
-        private System.Windows.Forms.TextBox txtDCoeff;
-        private System.Windows.Forms.TextBox txtOCostante;
-        private System.Windows.Forms.TextBox txtOCoeff;
-        private System.Windows.Forms.TextBox txtOEsponente;
-
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+
+
+        private System.Windows.Forms.TextBox txtDomA;
+        private System.Windows.Forms.TextBox txtDomB;
+        private System.Windows.Forms.TextBox txtOffA;
+        private System.Windows.Forms.TextBox txtOffB;
+        private System.Windows.Forms.TextBox txtMaxQ; 
+        private System.Windows.Forms.Label lblEquilibrioQ;
+        private System.Windows.Forms.Label lblEquilibrioP;
+
 
         private System.Windows.Forms.Button btnCalcola;
+
+        private System.Windows.Forms.DataGridView dgvValori;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
 
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
+
+
+
         private void InitializeComponent()
         {
-            this.txtDOrigine = new System.Windows.Forms.TextBox();
-            this.txtDCoeff = new System.Windows.Forms.TextBox();
-            this.txtOCostante = new System.Windows.Forms.TextBox();
-            this.txtOCoeff = new System.Windows.Forms.TextBox();
-            this.txtOEsponente = new System.Windows.Forms.TextBox();
-
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblEquilibrioQ = new System.Windows.Forms.Label();
+            this.lblEquilibrioP = new System.Windows.Forms.Label();
+            this.txtDomA = new System.Windows.Forms.TextBox();
+            this.txtDomB = new System.Windows.Forms.TextBox();
+            this.txtOffA = new System.Windows.Forms.TextBox();
+            this.txtOffB = new System.Windows.Forms.TextBox();
+            this.txtMaxQ = new System.Windows.Forms.TextBox();
             this.btnCalcola = new System.Windows.Forms.Button();
+            this.dgvValori = new System.Windows.Forms.DataGridView();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvValori)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
-
-            // ------------------- LABELS -------------------
-            this.label1.Text = "Domanda - Origine:";
+            // 
+            // label1
+            // 
             this.label1.Location = new System.Drawing.Point(20, 20);
-
-            this.label2.Text = "Domanda - Coeff:";
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Domanda A:";
+            // 
+            // label2
+            // 
             this.label2.Location = new System.Drawing.Point(20, 60);
-
-            this.label3.Text = "Offerta - Costante:";
-            this.label3.Location = new System.Drawing.Point(250, 20);
-
-            this.label4.Text = "Offerta - Coeff:";
-            this.label4.Location = new System.Drawing.Point(250, 60);
-
-            this.label5.Text = "Offerta - Esponente:";
-            this.label5.Location = new System.Drawing.Point(250, 100);
-
-            // ------------------- TEXTBOX -------------------
-            this.txtDOrigine.Location = new System.Drawing.Point(150, 20);
-            this.txtDOrigine.Size = new System.Drawing.Size(70, 23);
-            this.txtDOrigine.Text = "90";
-
-            this.txtDCoeff.Location = new System.Drawing.Point(150, 60);
-            this.txtDCoeff.Size = new System.Drawing.Size(70, 23);
-            this.txtDCoeff.Text = "4";
-
-            this.txtOCostante.Location = new System.Drawing.Point(370, 20);
-            this.txtOCostante.Size = new System.Drawing.Size(70, 23);
-            this.txtOCostante.Text = "10";
-
-            this.txtOCoeff.Location = new System.Drawing.Point(370, 60);
-            this.txtOCoeff.Size = new System.Drawing.Size(70, 23);
-            this.txtOCoeff.Text = "0.01";
-
-            this.txtOEsponente.Location = new System.Drawing.Point(370, 100);
-            this.txtOEsponente.Size = new System.Drawing.Size(70, 23);
-            this.txtOEsponente.Text = "3";
-
-            // ------------------- BOTTONE -------------------
-            this.btnCalcola.Text = "Calcola";
-            this.btnCalcola.Location = new System.Drawing.Point(20, 100);
-            this.btnCalcola.Size = new System.Drawing.Size(200, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Domanda B:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(20, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Offerta A:";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(20, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Offerta B:";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(20, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Max Q:";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(221, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tabella Valori / Grafico";
+            // 
+            // txtDomA
+            // 
+            this.txtDomA.Location = new System.Drawing.Point(120, 20);
+            this.txtDomA.Name = "txtDomA";
+            this.txtDomA.Size = new System.Drawing.Size(80, 20);
+            this.txtDomA.TabIndex = 6;
+            this.txtDomA.Text = "90";
+            // 
+            // txtDomB
+            // 
+            this.txtDomB.Location = new System.Drawing.Point(120, 60);
+            this.txtDomB.Name = "txtDomB";
+            this.txtDomB.Size = new System.Drawing.Size(80, 20);
+            this.txtDomB.TabIndex = 7;
+            this.txtDomB.Text = "4";
+            // 
+            // txtOffA
+            // 
+            this.txtOffA.Location = new System.Drawing.Point(120, 100);
+            this.txtOffA.Name = "txtOffA";
+            this.txtOffA.Size = new System.Drawing.Size(80, 20);
+            this.txtOffA.TabIndex = 8;
+            this.txtOffA.Text = "10";
+            // 
+            // txtOffB
+            // 
+            this.txtOffB.Location = new System.Drawing.Point(120, 140);
+            this.txtOffB.Name = "txtOffB";
+            this.txtOffB.Size = new System.Drawing.Size(80, 20);
+            this.txtOffB.TabIndex = 9;
+            this.txtOffB.Text = "100";
+            // 
+            // txtMaxQ
+            // 
+            this.txtMaxQ.Location = new System.Drawing.Point(120, 180);
+            this.txtMaxQ.Name = "txtMaxQ";
+            this.txtMaxQ.Size = new System.Drawing.Size(80, 20);
+            this.txtMaxQ.TabIndex = 10;
+            this.txtMaxQ.Text = "15";
+            // 
+            // btnCalcola
+            // 
+            this.btnCalcola.Location = new System.Drawing.Point(20, 215);
+            this.btnCalcola.Name = "btnCalcola";
+            this.btnCalcola.Size = new System.Drawing.Size(180, 35);
+            this.btnCalcola.TabIndex = 11;
+            this.btnCalcola.Text = "Calcola e Disegna";
             this.btnCalcola.Click += new System.EventHandler(this.btnCalcola_Click);
+            // 
+            // dgvValori
+            // 
+            this.dgvValori.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.dgvValori.Location = new System.Drawing.Point(20, 260);
+            this.dgvValori.Name = "dgvValori";
+            this.dgvValori.Size = new System.Drawing.Size(350, 300);
+            this.dgvValori.TabIndex = 12;
 
-            // ------------------- CHART -------------------
-            this.chart1.Location = new System.Drawing.Point(20, 150);
-            this.chart1.Size = new System.Drawing.Size(600, 350);
+            this.lblEquilibrioQ.Location = new System.Drawing.Point(20, 570);
+            this.lblEquilibrioQ.Size = new System.Drawing.Size(250, 23);
+            this.lblEquilibrioQ.Text = "Q equilibrio = ";
+            this.lblEquilibrioQ.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
 
-            // ------------------- FORM -------------------
-            this.ClientSize = new System.Drawing.Size(650, 520);
+            // lblEquilibrioP
+            this.lblEquilibrioP.Location = new System.Drawing.Point(300, 570);
+            this.lblEquilibrioP.Size = new System.Drawing.Size(250, 23);
+            this.lblEquilibrioP.Text = "Prezzo equilibrio = ";
+            this.lblEquilibrioP.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+
+            // 
+            // chart1
+            // 
+            this.chart1.Location = new System.Drawing.Point(400, 20);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(700, 540);
+            this.chart1.TabIndex = 13;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1150, 600);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label5);
-
-            this.Controls.Add(this.txtDOrigine);
-            this.Controls.Add(this.txtDCoeff);
-            this.Controls.Add(this.txtOCostante);
-            this.Controls.Add(this.txtOCoeff);
-            this.Controls.Add(this.txtOEsponente);
-
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDomA);
+            this.Controls.Add(this.txtDomB);
+            this.Controls.Add(this.txtOffA);
+            this.Controls.Add(this.txtOffB);
+            this.Controls.Add(this.txtMaxQ);
             this.Controls.Add(this.btnCalcola);
+            this.Controls.Add(this.dgvValori);
             this.Controls.Add(this.chart1);
-
-            this.Text = "Domanda e Offerta";
+            this.Controls.Add(this.lblEquilibrioQ);
+            this.Controls.Add(this.lblEquilibrioP);
+            this.Name = "Form1";
+            this.Text = "Curve Domanda e Offerta";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvValori)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
     }
+    
 }
 
       
--- prezzo equilibrio/Form1.cs
+++ prezzo equilibrio/Form1.cs
@@ -16,40 +16,154 @@ namespace prezzo_equilibrio
         public Form1()
         {
             InitializeComponent();
+
+ 
+
         }
+        
+
 
         private void btnCalcola_Click(object sender, EventArgs e)
         {
-            // Parametri inseriti dall'utente
-            double dOrigine = double.Parse(txtDOrigine.Text);
-            double dCoeff = double.Parse(txtDCoeff.Text);
-
-            double oCostante = double.Parse(txtOCostante.Text);
-            double oCoeff = double.Parse(txtOCoeff.Text);
-            double oEsponente = double.Parse(txtOEsponente.Text);
-
-            // Pulizia grafico
-            chart1.Series.Clear();
-
-            Series domanda = new Series("Domanda");
-            domanda.ChartType = SeriesChartType.Line;
-
-            Series offerta = new Series("Offerta");
-            offerta.ChartType = SeriesChartType.Line;
-
-            // Calcolo valori per q = 0..20
-            for (int q = 0; q <= 20; q++)
+            try
             {
-                double dValue = dOrigine - dCoeff * q;
-                double oValue = oCostante + oCoeff * Math.Pow(q, oEsponente);
+                // =====================
+                // LETTURA INPUT
+                // =====================
+                double domA = double.Parse(txtDomA.Text);
+                double domB = double.Parse(txtDomB.Text);
 
-                domanda.Points.AddXY(q, dValue);
-                offerta.Points.AddXY(q, oValue);
+                double offA = double.Parse(txtOffA.Text);
+                double offB = double.Parse(txtOffB.Text);
+
+                double maxQ = double.Parse(txtMaxQ.Text);
+
+                // =====================
+                // PULIZIE
+                // =====================
+                dgvValori.Rows.Clear();
+                chart1.Series.Clear();
+                chart1.ChartAreas.Clear();
+
+                ChartArea area = new ChartArea("area");
+                area.AxisX.Title = "Quantità (Q)";
+                area.AxisY.Title = "Prezzo";
+                area.AxisX.MajorGrid.LineColor = Color.LightGray;
+                area.AxisY.MajorGrid.LineColor = Color.LightGray;
+                area.AxisX.LineWidth = 2;
+                area.AxisY.LineWidth = 2;
+
+                chart1.ChartAreas.Add(area);
+
+                Series sDom = new Series("Domanda");
+                sDom.ChartType = SeriesChartType.Line;
+                sDom.BorderWidth = 2;
+                sDom.Color = Color.Blue;
+
+                Series sOff = new Series("Offerta");
+                sOff.ChartType = SeriesChartType.Line;
+                sOff.BorderWidth = 2;
+                sOff.Color = Color.Green;
+
+                chart1.Series.Add(sDom);
+                chart1.Series.Add(sOff);
+
+                double equilibrioQ = -1;
+                double equilibrioP = -1;
+                double minDiff = double.MaxValue;
+
+                double step = 0.001; // passo 0.001 per calcolo e tabella (3 decimali)
+
+                // =====================
+                // CALCOLO E POPOLAMENTO TABELLA + GRAFICO GENERALE
+                // =====================
+                for (double q = 0; q <= maxQ; q += step)
+                {
+                    double domanda = domA - domB * q;
+                    double offerta = offA + Math.Pow(q, 3) / offB;
+
+                    // tabella con 3 decimali
+                    dgvValori.Rows.Add(q.ToString("0.000"), domanda.ToString("0.000"), offerta.ToString("0.000"));
+
+                    // grafico
+                    sDom.Points.AddXY(q, domanda);
+                    sOff.Points.AddXY(q, offerta);
+
+                    // equilibrio
+                    double diff = Math.Abs(domanda - offerta);
+                    if (diff < minDiff)
+                    {
+                        minDiff = diff;
+                        equilibrioQ = Math.Round(q, 3);
+                        equilibrioP = Math.Round((domanda + offerta) / 2.0, 3);
+                    }
+                }
+
+                // =====================
+                // GRAFICO DETTAGLIATO INTORNO AL PUNTO DI EQUILIBRIO
+                // =====================
+                if (equilibrioQ >= 0)
+                {
+                    double range = 0.05;       // intervallo intorno all'equilibrio
+                    double stepDettaglio = 0.001; // passo fine 3 decimali
+
+                    Series sDomEq = new Series("Domanda Dettaglio");
+                    sDomEq.ChartType = SeriesChartType.Line;
+                    sDomEq.BorderWidth = 2;
+                    sDomEq.Color = Color.DarkBlue;
+
+                    Series sOffEq = new Series("Offerta Dettaglio");
+                    sOffEq.ChartType = SeriesChartType.Line;
+                    sOffEq.BorderWidth = 2;
+                    sOffEq.Color = Color.DarkGreen;
+
+                    for (double q = Math.Max(0, equilibrioQ - range); q <= equilibrioQ + range; q += stepDettaglio)
+                    {
+                        double domanda = domA - domB * q;
+                        double offerta = offA + Math.Pow(q, 3) / offB;
+
+                        sDomEq.Points.AddXY(Math.Round(q, 3), Math.Round(domanda, 3));
+                        sOffEq.Points.AddXY(Math.Round(q, 3), Math.Round(offerta, 3));
+                    }
+
+                    chart1.Series.Add(sDomEq);
+                    chart1.Series.Add(sOffEq);
+
+                    // =====================
+                    // PUNTO DI EQUILIBRIO
+                    // =====================
+                    Series sEq = new Series("Equilibrio");
+                    sEq.ChartType = SeriesChartType.Point;
+                    sEq.MarkerStyle = MarkerStyle.Circle;
+                    sEq.MarkerSize = 12;
+                    sEq.Color = Color.Red;
+                    sEq.Points.AddXY(equilibrioQ, equilibrioP);
+                    chart1.Series.Add(sEq);
+
+                    Series sLinea = new Series("Linea equilibrio");
+                    sLinea.ChartType = SeriesChartType.Line;
+                    sLinea.Color = Color.Red;
+                    sLinea.BorderDashStyle = ChartDashStyle.Dash;
+                    sLinea.BorderWidth = 2;
+                    sLinea.Points.AddXY(equilibrioQ, 0);
+                    sLinea.Points.AddXY(equilibrioQ, equilibrioP + 20);
+                    chart1.Series.Add(sLinea);
+
+                    // =====================
+                    // AGGIORNA LABEL SUL FORM
+                    // =====================
+                    lblEquilibrioQ.Text = $"Q equilibrio = {equilibrioQ:0.000}";
+                    lblEquilibrioP.Text = $"Prezzo equilibrio = {equilibrioP:0.000}";
+                }
             }
-
-            chart1.Series.Add(domanda);
-            chart1.Series.Add(offerta);
+            catch
+            {
+                MessageBox.Show("Errore: controlla che i valori numerici siano corretti.");
+            }
         }
     }
 }
+
+    
+
 
